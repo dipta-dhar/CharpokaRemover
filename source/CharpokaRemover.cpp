f4fa6c7006175ed5f211a6aec12195c441902884
Dipta Dip <30615936+dipta1010@users.noreply.github.com>
2019-03-10 13:21:05 +0600

    CharpokaRemover

---
+++ source/CharpokaRemover.cpp
@@ -0,0 +1,45 @@
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+using namespace std;
+
+
+int main()
+{
+    const TCHAR batchFilePath[MAX_PATH] = _T("C:\\Users\\dipta\\Desktop\\remover.bat");
+
+    TCHAR systemDirPath[MAX_PATH] = _T("");
+    GetSystemDirectory( systemDirPath, sizeof(systemDirPath)/sizeof(_TCHAR) );
+
+    // path to cmd.exe, path to batch file, plus some space for quotes, spaces, etc.
+    TCHAR commandLine[2 * MAX_PATH + 16] = _T("");
+
+    _sntprintf( commandLine, sizeof(commandLine)/sizeof(_TCHAR),
+        _T("\"%s\\cmd.exe\" /C \"%s\""), systemDirPath, batchFilePath );
+
+    STARTUPINFO si = {0}; // alternative way to zero array
+    si.cb = sizeof(si);
+    PROCESS_INFORMATION pi = {0};
+
+    if( !CreateProcess( NULL,
+        commandLine,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi )
+        )
+    {
+        _tprintf( _T("CreateProcess failed (%d)\n"), GetLastError() );
+        return FALSE;
+    }
+
+    WaitForSingleObject( pi.hProcess, INFINITE );
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+
+    return 0;
+}
